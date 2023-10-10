ba888f4ccd01180a03adb72f6d165c6959e5a257
JosiahPThompson <josiahpaulthompson@gmail.com>
2023-10-09 20:17:27 -0500

    added automated validation

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,9 +1,25 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "Username cannot be empty.")
+    @Size(min = 5, max = 15, message = "Size must be between 5 and 15 characters.")
     private String username;
+
+    @Email(message = "Must be a valid email.")
     private String email;
+
+    @NotBlank(message = "Must enter password.")
+    @Size(min = 6, message = "Password must have at least 6 characters.")
     private String password;
+
+    @NotNull(message = "Passwords do not match.")
+    private String verifyPassword;
 
     public User() {
 
@@ -14,6 +30,20 @@ public class User {
         this.username = username;
         this.email = email;
         this.password = password;
+    }
+
+    private void checkPassword(){
+        if (password != null && verifyPassword != null && !password.equals(verifyPassword)) {
+        setVerifyPassword(null);
+        }
+    }
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
     }
 
     public String getUsername() {
@@ -38,6 +68,7 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
     }
 }
 
